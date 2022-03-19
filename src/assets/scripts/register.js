af7fdb3dcf444e00f4bbe23c6ee529147fb36e1b
Parviz-Alakbarov <parvizja@code.edu.az>
2022-03-19 16:01:54 +0400

    dynamic select country feature added

--- src/assets/scripts/register.js
+++ src/assets/scripts/register.js
@@ -1,1 +1,48 @@
-console.log("register page")
+$(function(){
+	let countries= [];
+
+    getCountries();
+
+    $(document).on('change','#formCountry', function (e) {
+        console.log($('#formCity').html(`<option disabled selected aria-readonly="true">Şəhər seçin</option>`));
+        getCitiesByCountry(this.value)
+    });
+
+    async function getCountries() { 
+        await fetch('https://localhost:44337/api/settings/getCountries/')
+        .then(response => response.json())
+        .then(data => countries = data.data)
+        .then(data=>{
+            countries.forEach(element => {
+                $('#formCountry').append(`<option value="${element.id}">${element.name}</option>`);
+            })
+        })
+        .catch(error => {
+            throw(error);
+        });
+    }
+
+
+    async function getCitiesByCountry(countryId) { 
+        await fetch(`https://localhost:44337/api/settings/getCitiesByCountry/${countryId}`)
+        .then(response => response.json())
+        .then(data=>{
+            data.data.forEach(element => {
+                $('#formCity').append(`<option value="${element.id}">${element.name}</option>`);
+            })
+        })
+        .catch(error => {
+            throw(error);
+        });
+    }
+
+  
+    
+
+    
+
+    // countries.forEach(element => {
+    //     console.log(element.name)
+    // });
+
+  });
